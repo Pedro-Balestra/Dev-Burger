5be77590b6b1e95c249066d5cf95adf3ef82c709
Pedro <pedro.balestra@outlook.com>
2024-06-13 15:42:11 -0300

    14 - Criando Controller de Usuário

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,8 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-	const user = await User.create({
-		id: v4(),
-		name: 'Pedro',
-		email: 'pedro@outlook.com',
-		password_hash: 'asdasdasdda',
-	});
-
-	return res.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
