029c14090847c9d67e329fb0fefd5f8f6f2a59c1
Pedro <pedro.balestra@outlook.com>
2024-06-14 10:05:40 -0300

    18 - Fazendo o login de usurio

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -28,6 +28,10 @@ class User extends Model {
 		// biome-ignore lint/complexity/noThisInStatic: <explanation>
 		return this;
 	}
+
+	async checkPassword(password) {
+		return bcrypt.compare(password, this.password_hash);
+	}
 }
 
 export default User;
