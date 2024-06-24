c3bce2524ea76f9afaa1d8da7b506f978ef21564
Pedro <pedro.balestra@outlook.com>
2024-06-24 16:10:24 -0300

    06 - Iniciando a tela de Login

--- FrontEnd/devburger-interface/src/pages/Login/index.jsx
+++ FrontEnd/devburger-interface/src/pages/Login/index.jsx
@@ -1,7 +1,31 @@
+import Logo from '../../assets/logo.svg'
+import { Button, Container, From, InputContainer, LeftContainer, Link, RightContainer, Title } from "./styles"
+
 export function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt="logo-devbuger" />
+            </LeftContainer>
+            <RightContainer>
+                <Title>
+                    Olá, seja bem vindo ao <span>Dev Burguer!</span>
+                    Acesse com seu <span>Login e senha.</span>
+                </Title>
+                <From>
+                    <InputContainer>
+                        <label>email</label>
+                        <input type='email'></input>
+                    </InputContainer>
+                    <InputContainer>
+                        <label>senha</label>
+                        <input type='password'></input>
+                    </InputContainer>
+                    <Link>Esqueci minha senha.</Link>
+                    <Button>Entrar</Button>
+                </From>
+                <Link>Não possui conta? Clique aqui</Link>
+            </RightContainer>
+        </Container>
     )
 }
