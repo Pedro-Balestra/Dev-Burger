6bc3b71f2d59edceecc1059d2b1de7512e017f5e
Pedro <pedro.balestra@outlook.com>
2024-06-24 15:52:37 -0300

    05 - Estilos Globais com Styled-Components

--- FrontEnd/devburger-interface/src/main.jsx
+++ FrontEnd/devburger-interface/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Login } from './pages/Login/index.jsx'
+import GlobalStyles from './styles/globalStyles.js'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Login></Login>
+    <GlobalStyles />
   </React.StrictMode>,
 )
