6cd7349727b02ef7fcb17d41cc645fd05fd17711
Pedro <pedro.balestra@outlook.com>
2024-06-26 10:35:42 -0300

    09 - Formulário com React Hook Form

--- FrontEnd/devburger-interface/src/pages/Login/index.jsx
+++ FrontEnd/devburger-interface/src/pages/Login/index.jsx
@@ -1,33 +1,69 @@
-import Logo from '../../assets/logo.svg'
-import { Button } from '../../components/Button'
-import { Container, From, InputContainer, LeftContainer, RightContainer, Title } from "./styles"
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import Logo from "../../assets/logo.svg";
+import { Button } from "../../components/Button";
+import {
+	Container,
+	From,
+	InputContainer,
+	LeftContainer,
+	RightContainer,
+	Title,
+} from "./styles";
+
+const schema = yup
+	.object({
+		email: yup.string().email().required(),
+		password: yup.string().min(6).required(),
+	})
+	.required();
 
 export function Login() {
-    return (
-        <Container>
-            <LeftContainer>
-                <img src={Logo} alt="logo-devbuger" />
-            </LeftContainer>
-            <RightContainer>
-                <Title>
-                    Olá, seja bem vindo ao <span>Dev Burguer!</span>
-                    <br />
-                    Acesse com seu <span>Login e senha.</span>
-                </Title>
-                <From>
-                    <InputContainer>
-                        <label>Email</label>
-                        <input type='email' />
-                    </InputContainer>
-                    <InputContainer>
-                        <label>Senha</label>
-                        <input type='password' />
-                    </InputContainer>
-                    <Button>Entrar</Button>
-                </From>
-                {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
-                    <p>Não possui conta? <a>Clique aqui</a></p>
-            </RightContainer>
-        </Container>
-    )
+	const schema = yup
+		.object({
+			email: yup.string().email().required(),
+			password: yup.string().min(6).required(),
+		})
+		.required();
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
+	const onSubmit = (data) => console.log(data);
+
+	return (
+		<Container>
+			<LeftContainer>
+				<img src={Logo} alt="logo-devbuger" />
+			</LeftContainer>
+			<RightContainer>
+				<Title>
+					Olá, seja bem vindo ao <span>Dev Burguer!</span>
+					<br />
+					Acesse com seu <span>Login e senha.</span>
+				</Title>
+				<From onSubmit={handleSubmit(onSubmit)}>
+					<InputContainer>
+						<label>Email</label>
+						<input type="email" {...register("email")} />
+					</InputContainer>
+					<InputContainer>
+						<label>Senha</label>
+						<input type="password" {...register("password")} />
+					</InputContainer>
+					<Button>Entrar</Button>
+				</From>
+				<p>
+					Não possui conta?{" "}
+					{/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+					<a>Clique aqui</a>
+				</p>
+			</RightContainer>
+		</Container>
+	);
 }
