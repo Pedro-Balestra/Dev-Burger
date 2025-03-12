b9dddeaf8dba32a989e3fc272d485306202d3bba
Pedro <pedro.balestra@outlook.com>
2025-03-12 18:17:18 -0300

    19 - Atualizando o nosso banco de dados

---
+++ seed-devburger-api/src/config.js
@@ -0,0 +1,4 @@
+export const config = {
+  userToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmOTNjOWFmLTYzMjQtNDUzMS1iMDVmLWVjZWU4NWVkNjQyZSIsIm5hbWUiOiJQZWRybyIsImlhdCI6MTc0MTgxMzkzMiwiZXhwIjoxNzQyNDE4NzMyfQ.oKCvhvebB7A9mldTOamY4NwuJQ2zuWjRHpDzwGSK-5g',
+  apiUrl: 'http://localhost:3001'
+}
