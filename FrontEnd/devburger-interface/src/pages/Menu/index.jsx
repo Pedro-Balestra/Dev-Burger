4b702e39b9947d47982770e9f9505f8ba7c52765
Pedro <pedro.balestra@outlook.com>
2025-03-19 18:20:32 -0300

    28 - Criando o Menu

---
+++ FrontEnd/devburger-interface/src/pages/Menu/index.jsx
@@ -0,0 +1,17 @@
+import { Container, Banner, CategoryMenu, ProductsContainer } from './styles';
+
+export function Menu(){
+    return(
+        <Container>
+            <Banner>
+                <h1>O MELHOR <br />HAMBUERGER <br />ESTÁ AQUI! <span>Esse cardápio está irresistível!</span></h1>
+            </Banner>
+            <CategoryMenu>
+
+            </CategoryMenu>
+            <ProductsContainer>
+
+            </ProductsContainer>
+        </Container>
+    )
+}
--- FrontEnd/devburger-interface/src/routes/index.jsx
+++ FrontEnd/devburger-interface/src/routes/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Login } from '../pages/Login';
 import { Register } from '../pages/Register';
 import { Home } from '../pages/Home';
+import { Menu } from '../pages/Menu';
 
 export const router = createBrowserRouter([
 	{
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
 	{
 		path: '/cadastro',
 		element: <Register />,
+	},
+	{
+		path: '/cardapio',
+		element: <Menu />,
 	},
 ]);
