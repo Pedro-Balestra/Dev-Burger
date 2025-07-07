569b2f29503dbf2ab2bf4b1e65b8379df456f0ad
Pedro Gabriel Garcia Ribeiro Balestra <pedro.g@inatel.br>
2025-07-07 18:32:27 -0300

    54 - Criando Menu lateral

--- FrontEnd/devburger-interface/src/components/SideNavAdmin/index.jsx
+++ FrontEnd/devburger-interface/src/components/SideNavAdmin/index.jsx
@@ -1,10 +1,29 @@
+import { SignOut } from '@phosphor-icons/react';
+import Logo from '../../assets/logo.svg';
+import { useUser } from '../../hooks/UserContext';
+import { navLinks } from './navLinks';
+import { Container, Footer, NavLink, NavLinkContainer } from './styles';
+
 export function SideNavAdmin() {
+    const { logout } = useUser();
     return (
-        <div>
-            <h3>
-                Menu
-            </h3>
-        </div>
+        <Container>
+            <img src={Logo} alt="Hamburger logo"></img>
+            <NavLinkContainer>
+                {navLinks.map(link => (
+                    <NavLink key={link.id} to={link.path}>
+                        {link.icon}
+                        <span>{link.label}</span>
+                    </NavLink>
+                ))}
+            </NavLinkContainer>
+            <Footer>
+                <NavLink to='/login' onClick={logout}>
+                    <SignOut />
+                    <span>Sair</span>
+                </NavLink>
+            </Footer>
+        </Container>
 
     );
 }
